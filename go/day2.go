8478768e6f7b4aed6a19b1e5441ed43518afd921
bgaudino <bgaudino@gmail.com>
2022-12-02 13:34:51 -0600

    Simpler go solution

--- go/day2.go
+++ go/day2.go
@@ -7,76 +7,59 @@ import (
 	"strings"
 )
 
-type shape struct {
-	name     string
-	points   int
-	beats    string
-	beatenBy string
-}
+var rock = 1
+var paper = 2
+var scissors = 3
 
-type outcome struct {
-	name   string
-	points int
-}
+var loss = 0
+var draw = 3
+var win = 6
 
-var rock = shape{name: "rock", points: 1, beats: "scissors", beatenBy: "paper"}
-var paper = shape{name: "paper", points: 2, beats: "rock", beatenBy: "scissors"}
-var scissors = shape{name: "scissors", points: 3, beats: "paper", beatenBy: "rock"}
+var elfCodes = map[string]int{"A": rock, "B": paper, "C": scissors}
+var myCodes = map[string]int{"X": rock, "Y": paper, "Z": scissors}
+var outcomeCodes = map[string]int{"X": loss, "Y": draw, "Z": win}
 
-var elfShapes = map[string]shape{"A": rock, "B": paper, "C": scissors}
-var myShapes = map[string]shape{"X": rock, "Y": paper, "Z": scissors}
-var myOutcomes = map[string]outcome{"X": loss, "Y": draw, "Z": win}
-
-var win = outcome{name: "win", points: 6}
-var draw = outcome{name: "draw", points: 3}
-var loss = outcome{name: "loss", points: 0}
-
-func (s1 shape) getOutcomeFromOpponentShape(s2 shape) outcome {
-	if s1.beats == s2.name {
-		return win
-	}
-	if s1.name == s2.name {
-		return draw
-	}
-	return loss
-}
-
-func (s shape) getShapeFromOpponentOutcome(o outcome) shape {
-	shapes := map[string]shape{
-		"rock":     rock,
-		"paper":    paper,
-		"scissors": scissors,
-	}
-	if o.name == "win" {
-		return shapes[s.beatenBy]
-	}
-	if o.name == "loss" {
-		return shapes[s.beats]
-	}
-	return shapes[s.name]
-}
+var shapes = []int{1, 2, 3}
 
 func main() {
-
 	file, _ := os.Open("../data/day2.txt")
 	scanner := bufio.NewScanner(file)
 
 	partOnePoints := 0
 	partTwoPoints := 0
 	for scanner.Scan() {
-		shapes := strings.Split(scanner.Text(), " ")
+		codes := strings.Split(scanner.Text(), " ")
 
-		// Part one
-		elfShape, myShape := elfShapes[shapes[0]], myShapes[shapes[1]]
-		myOutcome := myShape.getOutcomeFromOpponentShape(elfShape)
-		partOnePoints += myShape.points + myOutcome.points
+		elf, me := elfCodes[codes[0]], myCodes[codes[1]]
+		partOnePoints += me + getOutcomePoints(elf, me)
 
-		// Part two
-		myOutcome = myOutcomes[shapes[1]]
-		myShape = elfShape.getShapeFromOpponentOutcome(myOutcome)
-		partTwoPoints += myShape.points + myOutcome.points
+		outcome := outcomeCodes[codes[1]]
+		partTwoPoints += outcome + getShapePointsByOutcome(elf, outcome)
 	}
 
 	fmt.Println(partOnePoints)
 	fmt.Println(partTwoPoints)
+}
+
+func getOutcomePoints(s1 int, s2 int) int {
+	if s1 == s2 {
+		return draw
+	}
+	if s1%3+1 == s2 {
+		return win
+	}
+	return loss
+}
+
+func getShapePointsByOutcome(s int, o int) int {
+	if o == draw {
+		return s
+	}
+	if o == win {
+		return s%3 + 1
+	}
+	if s-1 == 0 {
+		return 3
+	}
+	return s - 1
 }
