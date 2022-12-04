f888eca38dddfd6afc7031ffdd1af8fdf6878970
bgaudino <bgaudino@gmail.com>
2022-12-04 11:36:35 -0600

    Get path to data file

--- python/utils.py
+++ python/utils.py
@@ -1,4 +1,8 @@
+from pathlib import Path
+
+
 def get_data(day):
-    filename = f'data/day{day}.txt'
+    path = Path(__file__).parent.parent
+    filename = f'{path}/data/day{day}.txt'
     with open(filename, 'r') as file:
         return file.read().split('\n')
