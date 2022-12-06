8b07a61e201e0fe96154494e110918f2c482134e
bgaudino <bgaudino@gmail.com>
2022-12-05 22:35:05 -0600

    Update python tests

--- python/tests.py
+++ python/tests.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import day1
 import day2
 import day3
@@ -5,9 +7,40 @@ import day4
 import day5
 
 
-assert day1.main() == (72070, 211805)
-assert day2.main() == (14297, 10498)
-assert day3.main() == (7826, 2577)
-assert day4.main() == (424, 804)
-assert day5.main() == ('FJSRQCFTN', 'CJVLJQPHS')
-print('ok')
+@dataclass
+class Day:
+    solution: __module__
+    part1: str
+    part2: str
+
+
+days = [
+    Day(day1, 72070, 211805),
+    Day(day2, 14297, 10498),
+    Day(day3, 7826, 2577),
+    Day(day4, 424, 804),
+    Day(day5, 'FJSRQCFTN', 'CJVLJQPHS'),
+]
+
+total_errors = 0
+for i, day in enumerate(days):
+    errors = 0
+    part1, part2 = day.solution.main()
+    try:
+        assert part1 == day.part1
+    except AssertionError:
+        errors += 1
+        print(f'Day {i + 1} (part 1): Expected {day.part1} got {part1}')
+    else:
+        print(f'Day {i + 1} (part 1): OK')
+    try:
+        assert part2 == day.part2
+    except AssertionError:
+        errors += 1
+        print(f'Day {i + 1} (part 2): Expected {day.part2} got {part2}')
+    else:
+        print(f'Day {i + 1} (part 2): OK')
+    total_errors += errors
+
+num_tests = len(days) * 2
+print(f'Ran {num_tests}: {num_tests - total_errors} passed, {total_errors} failed.')
