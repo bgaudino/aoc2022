03bd5784c7a996291d483bea40710683438d00b6
bgaudino <bgaudino@gmail.com>
2022-12-07 16:39:39 -0600

    Refactor python solution

--- python/day7.py
+++ python/day7.py
@@ -1,76 +1,58 @@
 from utils import get_data
 
 
-class FileSystem:
-    def __init__(self):
-        self.dirs = {'/': {}}
-        self.cwd = '/'
-        self.path = ['/']
-        self.is_listing = False
-        self.current_dir = {}
+class Directory:
+    def __init__(self, parent=None):
+        self.files = []
+        self.parent = parent
+        self.children = {}
 
-    def update_filesystem(self):
-        fs = self.dirs
-        for i, p in enumerate(self.path):
-            if i == len(self.path) - 1:
-                fs[p] = self.current_dir
-                break
-            fs = fs[p]
 
-    def process_command(self, command):
-        if command.startswith('$ cd'):
-            if self.is_listing:
-                self.update_filesystem()
-            self.is_listing = False
-            cwd = command.split(' ')[-1]
-            self.cd(cwd)
-            self.current_dir = {}
+def dir_sizes(directory):
+    sizes = []
 
-        if command.startswith('$ ls'):
-            self.is_listing = True
+    def dir_size(dir):
+        size = sum(dir.files)
+        size += sum([dir_size(c) for c in dir.children.values()])
+        sizes.append(size)
+        return size
 
-    def cd(self, dir):
-        if dir == '/':
-            self.path = ['/']
-        elif dir == '..':
-            self.path.pop()
-        else:
-            self.path.append(dir)
-
-    def add_item(self, data):
-        if data.startswith('dir'):
-            key = data.split(' ')[-1]
-            if key not in self.current_dir:
-                self.current_dir[key] = {}
-        else:
-            size, file = data.split(' ')
-            self.current_dir[file] = int(size)
-
-    def dir_sizes(self):
-        sizes = []
-
-        def dir_size(dir):
-            size = sum([v for v in dir.values() if type(v) == int])
-            size += sum(
-                [dir_size(v) for v in dir.values() if type(v) == dict]
-            )
-            sizes.append(size)
-            return size
-
-        return dir_size(self.dirs['/']), sizes
+    return dir_size(directory), sizes
 
 
 def main():
-    f = FileSystem()
+    root = Directory('/')
+    cursor = root
     for line in get_data(7):
-        if line.startswith('$'):
-            f.process_command(line)
-        else:
-            f.add_item(line)
-    if f.is_listing:
-        f.update_filesystem()
+        parts = line.split(' ')
+        if line.startswith('dir'):
+            name = parts[-1]
+            cursor.children[name] = Directory(parent=cursor)
+            continue
+        if not line.startswith('$'):
+            size = int(parts[0])
+            cursor.files.append(size)
+            continue
+        if line.startswith('$ ls'):
+            continue
+        if parts[2] == "/":
+            cursor = root
+            continue
+        if parts[2] == "..":
+            cursor = cursor.parent
+            continue
 
-    total_size, sizes = f.dir_sizes()
+        # cd
+        name = parts[2]
+        if d := cursor.children.get(name):
+            cursor = d
+            continue
+
+        d = Directory(parent=cursor)
+        cursor.children[name] = d
+        cursor = d
+
+    total_size, sizes = dir_sizes(root)
     available_space = 70000000 - total_size
     space_to_free = 30000000 - available_space
 
