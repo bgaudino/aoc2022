ddf09f781cd7db7c0905ae44ec286b255e085661
bgaudino <bgaudino@gmail.com>
2022-12-20 12:53:46 -0600

    Day 18 python part 2

--- python/day18.py
+++ python/day18.py
@@ -15,41 +15,58 @@ class LavaDroplet:
 
     @cached_property
     def start(self):
-        return min(min(point) for point in self.cubes) - 1
+        return (
+            min(x for x, _, _ in self.cubes) - 1,
+            min(y for _, y, _ in self.cubes) - 1,
+            min(z for _, _, z in self.cubes) - 1,
+        )
 
     @cached_property
     def end(self):
-        return min(min(point) for point in self.cubes) - 1
+        return (
+            max(x for x, _, _ in self.cubes) + 1,
+            max(y for _, y, _ in self.cubes) + 1,
+            max(z for _, _, z in self.cubes) + 1,
+        )
 
     def get_neighbors(self, cube):
         x, y, z = cube
-        return {c for dx, dy, dz in DELTAS if not self.out_of_bounds(c := (x + dx, y + dy, z + dz))}
+        return {(x + dx, y + dy, z + dz) for dx, dy, dz in DELTAS}
 
     def out_of_bounds(self, cube):
-        for n in cube:
-            if not self.start < n < self.end:
-                return False
-        return True
+        return any(n < self.start[i] or n > self.end[i] for i, n in enumerate(cube))
 
     @cached_property
     def open_sides(self):
-        sides = []
-        for cube in self.cubes:
-            sides.extend(
-                [n for n in self.get_neighbors(cube) if n not in self.cubes]
-            )
-        return sides
+        return [neighbor for cube in self.cubes for neighbor in self.get_neighbors(cube) if neighbor not in self.cubes]
 
     @cached_property
     def surface_area(self):
         return len(self.open_sides)
+
+    @cached_property
+    def outside_surface_area(self):
+        outside_cubes = set()
+        candidates = [self.start]
+        while candidates:
+            cube = candidates.pop()
+            outside_cubes.add(cube)
+            for neighbor in self.get_neighbors(cube):
+                if self.out_of_bounds(neighbor):
+                    continue
+                if neighbor in self.cubes or neighbor in outside_cubes:
+                    continue
+                candidates.append(neighbor)
+        return sum(
+            neighbor in outside_cubes for cube in self.cubes for neighbor in self.get_neighbors(cube)
+        )
 
 
 def main():
     lines = get_data(18)
     cubes = {tuple(([int(n) for n in line.split(',')])) for line in lines}
     droplet = LavaDroplet(cubes)
-    return droplet.surface_area
+    return droplet.surface_area, droplet.outside_surface_area
 
 
 if __name__ == '__main__':
